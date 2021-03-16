f9a7697f77becc84f99f475aae342ec56b79ac76
IevgenDilevskyi <e.dilevskiy@gmail.com>
2021-03-16 18:59:43 +0000

    minor changes

--- express_server.js
+++ express_server.js
@@ -10,8 +10,8 @@ function generateRandomString(length=6){
 };
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": "www.lighthouselabs.ca",
+  "9sm5xK": "www.google.com"
 };
 
 app.get("/", (req, res) => {
@@ -28,12 +28,18 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  console.log(req.body);
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = `https://${urlDatabase[req.params.shortURL]}`;
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
