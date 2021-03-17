3c5161574312279ac120fe30a1adfd1d303e1fa9
IevgenDilevskyi <e.dilevskiy@gmail.com>
2021-03-17 20:41:55 +0000

    implemented creation of a new user and setting new cookie during registration. Changed templateVars to pass new user object to templates

--- express_server.js
+++ express_server.js
@@ -15,6 +15,18 @@ const urlDatabase = {
   "b2xVn2": "www.lighthouselabs.ca",
   "9sm5xK": "www.google.com"
 };
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -23,15 +35,20 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const templateVars = { urls: urlDatabase, user: users[req.cookies.user_id] };// user: users[req.cookies.user_id] - user from the table with current cookies.user_id
+  console.log("tVArs", templateVars)
   res.render("urls_index", templateVars);
 });
 app.get("/register", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = { user: users[req.cookies.user_id] };
   res.render("register", templateVars);
 });
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = { user: users[req.cookies.user_id] };
+  const id = req.cookies.user_id;
+  console.log(id);
+  const test = { user: users[id] };
+  console.log("test", test);
   res.render("urls_new", templateVars);
 });
 app.post("/urls", (req, res) => {
@@ -58,8 +75,18 @@ app.post("/logout", (req, res) => {
   res.clearCookie("username");
   res.redirect("/urls");
 });
+app.post("/register", (req, res) => {
+  const id = generateRandomString();
+  const email = req.body.email;
+  const password = req.body.password;
+  users[id] = { id, email, password };
+  res.cookie("user_id", id);
+  // console.log("users", users[id]);
+  console.log("user  ", req.cookies.user_id);
+  res.redirect("/urls");
+});
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies.user_id] };
   res.render("urls_show", templateVars);
 });
 app.get("/u/:shortURL", (req, res) => {
