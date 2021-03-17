5c3c3779aacc739065e72d3c4a1bb98640b642d4
IevgenDilevskyi <e.dilevskiy@gmail.com>
2021-03-17 22:48:56 +0000

    removed login functionality from _header.ejs and moved login.ejs file

--- express_server.js
+++ express_server.js
@@ -86,6 +86,7 @@ app.post("/login", (req, res) => {
 });
 app.post("/logout", (req, res) => {
   res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
 app.post("/register", (req, res) => {
